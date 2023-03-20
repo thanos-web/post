e06be9f78cb06d88c2e888fe8a35b8e2fee6a38c
Tanos <tatsday@gmail.com>
2023-03-20 21:59:08 +0300

    postlist added

--- src/сomponents/app-post/index.jsx
+++ src/сomponents/app-post/index.jsx
@@ -1,6 +1,7 @@
 import { CssBaseline } from "@mui/material"
 import { Container } from "@mui/system";
 import { AppHeader } from '../app-header';
+import { PostList } from "../postList";
 
 export const AppPost = () => {
     return (
