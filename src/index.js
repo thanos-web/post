b6b509ce96107bdbfb27b15fd49adc7d816b6bbf
Tanos <tatsday@gmail.com>
2023-03-20 14:53:40 +0300

    Добавила папку апи и подключила в js файл

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { AppPost } from './сomponents/app-post';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <AppPost/>
   </React.StrictMode>
 );
 
