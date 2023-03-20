d5c21aa7bc02b0084c6dd7892c597c9050cec939
Tanos <tatsday@gmail.com>
2023-03-20 21:54:09 +0300

    fix strange error

--- src/сomponents/app-post/index.jsx
+++ src/сomponents/app-post/index.jsx
@@ -1,12 +1,16 @@
 import { CssBaseline } from "@mui/material"
 import { Container } from "@mui/system";
 import { AppHeader } from '../app-header';
+import { PostList } from "../postList";
+
+
 export const AppPost = () => {
     return (
         <>
         <CssBaseline/>
         <Container>
         <AppHeader/>
+        <PostList/>
         </Container>
         
         </>
