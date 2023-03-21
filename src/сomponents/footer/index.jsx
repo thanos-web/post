fe5a91d4976e592db8cb0048dd571ca9ba3d6f08
Tanos <tatsday@gmail.com>
2023-03-21 10:51:59 +0300

    gfdgfdgf

---
+++ src/сomponents/footer/index.jsx
@@ -0,0 +1,21 @@
+import { Facebook, Instagram, Telegram } from "@mui/icons-material";
+import { BottomNavigation, BottomNavigationAction } from "@mui/material";
+import { Box } from "@mui/system";
+import s from "./style.module.css";
+
+
+
+
+
+export const Footer = () => {
+  
+    return (
+        <Box >
+            <BottomNavigation className={s.footer}>
+                <BottomNavigationAction label="Recents" icon={<Telegram />} />
+                <BottomNavigationAction label="Favorites" icon={<Instagram />} />
+                <BottomNavigationAction label="Nearby" icon={<Facebook/>} />
+            </BottomNavigation>
+        </Box>
+    )
+  }
