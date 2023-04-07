3bc6a90731bcd7a0c4627c86dfa4778877b7d487
Tanos <tatsday@gmail.com>
2023-04-07 13:05:34 +0300

    button changes

---
+++ src/сomponents/button/index.jsx
@@ -0,0 +1,14 @@
+import { Button, Stack } from "@mui/material";
+
+export const ButtonAdd = () => {
+  const handleOpenModal = () => {
+    console.log("Есть контакт");
+  };
+  
+  
+    return (
+        <Stack spacing={2} direction="row">
+            <Button variant="text" color="inherit" onClick={handleOpenModal}>Добавить пост</Button>
+        </Stack>
+    )
+    }
